773dd1fc837a43215493c4dcb3097de5766aab6b
coder676byte <daniell1303@icloud.com>
2025-02-21 18:45:11 +0000

    still script testing

---
+++ source/main.js
@@ -0,0 +1,4 @@
+import { title } from "./title_screen.js";
+
+// Option 2: To be extra sure, use this:
+document.addEventListener('DOMContentLoaded', title);
---
+++ source/title_screen.js
@@ -0,0 +1,4 @@
+export function title() {
+    let titleScreen = document.getElementById("Title_screen");
+    let button = document.getElementById("start");
+    button.style.visibility = "hidden"}
