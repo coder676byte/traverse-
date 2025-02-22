056580151652a4e8a936ccd45acce00386b818cf
coder676byte <daniell1303@icloud.com>
2025-02-22 18:32:05 +0000

    trying to work out weather the html is actually reading the code files

--- title_screen.js
+++ title_screen.js
@@ -1,10 +1,10 @@
 export function title(click) {
   let titleScreen = document.getElementById("Title_screen");
   let button = document.getElementById("start");
+  titleScreen.style.visibility = "hidden"
   button.addEventListener("click", function() {
     titleScreen.style.visibility = "hidden";
       }
   );
 }
 
-title(1)
