39ee74abad72f914b8ab654e6d230b6509f10d35
coder676byte <daniell1303@icloud.com>
2025-02-22 17:53:42 +0000

    forcing the bug to happen

--- title_screen.js
+++ title_screen.js
@@ -2,8 +2,7 @@ export function title(click) {
   let titleScreen = document.getElementById("Title_screen");
   let button = document.getElementById("start");
   button.addEventListener("click", function() {
-      if (click == 1) {
-          titleScreen.style.visibility = "hidden";
+    titleScreen.style.visibility = "hidden";
       }
-  });
+  );
 }
