618d519f5d27c4c32064c6ea8ce62f04dc2b0bf8
coder676byte <daniell1303@icloud.com>
2025-02-22 20:50:07 +0000

    fixing import bug

--- title_screen.js
+++ title_screen.js
@@ -1,4 +1,4 @@
-function title(click) {
+export function title(click) {
   let titleScreen = document.getElementById("Title_screen");
   let button = document.getElementById("start");
   titleScreen.style.visibility = "hidden"
